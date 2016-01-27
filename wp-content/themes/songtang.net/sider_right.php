e9a2055d86cacc1dd74522b57e2255d4414d2819
doDomoGu <dodomo_gu@sina.cn>
2016-01-27 18:47:00 +0800

    图片替换

--- wp-content/themes/songtang.net/sider_right.php
+++ wp-content/themes/songtang.net/sider_right.php
@@ -1,9 +1,9 @@
-<a href="/" target="_blank">
-    <img width="300px" alt="" src="<?php bloginfo('template_url')?>/static/img/test/intro_right_1.jpg">
+<a target="_blank">
+    <img width="300px" alt="" src="<?php bloginfo('template_url')?>/static/img/common/intro_right_1.jpg">
 </a>
-<a href="/" target="_blank">
-    <img width="300px" alt="" src="<?php bloginfo('template_url')?>/static/img/test/intro_right_2.jpg">
+<a target="_blank">
+    <img width="300px" alt="" src="<?php bloginfo('template_url')?>/static/img/common/intro_right_2.jpg">
 </a>
-<a href="/" target="_blank">
-    <img width="300px" alt="" src="<?php bloginfo('template_url')?>/static/img/test/intro_right_3.jpg">
+<a target="_blank">
+    <img width="300px" alt="" src="<?php bloginfo('template_url')?>/static/img/common/intro_right_3.jpg">
 </a>
