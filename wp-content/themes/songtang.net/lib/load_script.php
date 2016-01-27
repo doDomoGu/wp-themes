21b692f1ebaf4f217d60ee03bf0f747424d486a7
doDomoGu <dodomo_gu@sina.cn>
2016-01-27 11:37:24 +0800

    整理 css 和 js 文件的载入

--- wp-content/themes/songtang.net/lib/load_script.php
+++ wp-content/themes/songtang.net/lib/load_script.php
@@ -1,61 +1,71 @@
 <?php
     //css
     $cssDir = '/static/css/';
-    $cssFile = false;
-    if(is_home())
-        $cssFile = 'home.css';
-    if(is_search())
-        $cssFile = 'search.css';
-    if(is_single())
-        $cssFile = 'single.css';
+    $cssFiles = [];
+    if(is_home()){
+        $cssFiles[] = 'home.css';
+    }
+    if(is_search()){
+        //$cssFiles[] = 'search.css';
+    }
+    if(is_single()){
+        $cssFiles[] = 'single.css';
+    }
     if(is_page()){
         if('page_intro.php' == get_page_template_slug()){
-            $cssFile = 'page_intro.css';
+            $cssFiles[] = 'page_intro.css';
         }else{
-            $cssFile = 'page.css';
+            //$cssFiles[] = 'page.css';
         }
     }
-
-    if(is_category())
-        $cssFile = 'category.css';
-    if(is_month())
-        $cssFile = 'month.css';
+    if(is_category()){
+        $cssFiles[] = 'category.css';
+    }
+    if(is_month()){
+        //$cssFiles[] = 'month.css';
+    }
 ?>
-<?php if($cssFile!=false):?>
-<link rel="stylesheet" href="<?php bloginfo('template_url'); ?><?=$cssDir.$cssFile?>" type="text/css" media="screen" />
+<?php if(!empty($cssFiles)):?>
+<?php foreach($cssFiles as $css):?>
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?><?=$cssDir.$css?>" type="text/css" media="screen" />
+<?php endforeach;?>
 <?php endif;?>
 
 <?php
     //js
     $jsDir = '/static/js/';
-    $jsFile = false;
     $jsFiles = [];
-    if(is_home())
-        $jsFile = 'home.js';
-    if(is_search())
-        $jsFile = 'search.js';
-    if(is_single())
-        $jsFile = 'single.js';
+    if(is_home()){
+        $jsFiles[] = 'home.js';
+    }
+    if(is_search()){
+        //$jsFiles[] = 'search.js';
+    }
+    if(is_single()){
+        //$jsFiles[] = 'single.js';
+    }
     if(is_page()){
         if('page_intro.php' == get_page_template_slug()){
             $jsFiles[] = 'nav.js';
             //$jsFile = 'page_intro.js';
+        }elseif('page_contact.php' == get_page_template_slug()){
+
         }else{
             //$jsFile = 'page.js';
         }
     }
     if(is_category()){
-        $jsFiles[] = 'category.js';
+        //$jsFiles[] = 'category.js';
         $jsFiles[] = 'nav.js';
     }
-    if(is_month())
-        $jsFile = 'month.js';
+    if(is_month()){
+        //$jsFile = 'month.js';
+    }
+
 ?>
 <?php if(!empty($jsFiles)):?>
     <?php foreach($jsFiles as $js):?>
         <script src="<?php bloginfo('template_url'); ?><?=$jsDir.$js?>"></script>
     <?php endforeach;?>
-<?php elseif($jsFile!=false):?>
-    <script src="<?php bloginfo('template_url'); ?><?=$jsDir.$jsFile?>"></script>
 <?php endif;?>
 
