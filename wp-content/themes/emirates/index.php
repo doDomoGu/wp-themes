3905bd52e4d987519dad09cc808dce4290a7c420
doDomoGu <dodomo_gu@sina.cn>
2016-01-31 14:58:14 +0800

    first

---
+++ wp-content/themes/emirates/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+    <section id="container">
+        <div class="row">
+            <?php /*Bing_posts_list(); */?>
+            index
+        </div>
+    </section>
+<?php get_footer(); ?>
