75f126ceae6bd883f9a4d88282a83bb821f57e31
doDomoGu <dodomo_gu@sina.cn>
2016-02-01 22:18:57 +0800

    头部 底部 修改

--- wp-content/themes/emirates/footer.php
+++ wp-content/themes/emirates/footer.php
@@ -3,15 +3,15 @@
 			</div>
 			<footer id="footer">
 				<div class="box-md">
-					<nav class="menu footer-menu-box" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+					<!--<nav class="menu footer-menu-box" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 						<span class="menu-title"><span class="dashicons dashicons-menu"></span><?php /*echo Bing_menu_name( 'footer_menu' ); */?></span>
-						<?php wp_nav_menu( array(
+						<?php /*wp_nav_menu( array(
 							'theme_location' => 'footer_menu',
 							'container'      => false,
 							'items_wrap'     => '<ul id="footer-menu">%3$s</ul>',
 							'fallback_cb'    => 'Bing_empty_menu_fallback'
-						) ); ?>
-					</nav>
+						) ); */?>
+					</nav>-->
 					<!--<p class="footer-left"><?php /*echo Bing_mpanel( 'footer_text_left' ); */?></p>
 					<p class="footer-right"><?php /*echo Bing_mpanel( 'footer_text_right' ); */?></p>-->
 				</div>
--- wp-content/themes/emirates/header.php
+++ wp-content/themes/emirates/header.php
@@ -1,9 +1,12 @@
 <?php /*if( Bing_is_ajax_load_page() ) return; */?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html>
 	<head>
-		<meta charset="<?php echo esc_attr( get_bloginfo( 'charset' ) ); ?>" />
-		<link rel="pingback" href="<?php echo esc_attr( get_bloginfo( 'pingback_url' ) ); ?>" />
+        <meta http-equiv="Content-Type" content="text/html; charset=<? echo get_bloginfo('charset'); ?>" />
+        <title><? bloginfo('name'); ?></title>
+        <meta name="description" content="<? bloginfo('description'); ?>" />
+		<!--<link rel="pingback" href="<?php /*echo esc_attr( get_bloginfo( 'pingback_url' ) ); */?>" />-->
+        <link rel="stylesheet" href="<? bloginfo('stylesheet_url'); ?>" type="text/css" />
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
@@ -13,26 +16,26 @@
 					<h1 class="logo">
 						<a href="<?php echo esc_url( home_url() ) ?>" title="<?php echo esc_attr( sprintf( '%s | %s', get_bloginfo( 'name', 'display' ), get_bloginfo( 'description', 'display' ) ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 					</h1>
-					<nav class="menu header-menu-box" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-						<?php wp_nav_menu( array(
+					<!--<nav class="menu header-menu-box" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+						<?php /*wp_nav_menu( array(
 							'theme_location' => 'header_menu',
 							'container'      => false,
 							'items_wrap'     => '<ul id="header-menu">%3$s</ul>',
 							'fallback_cb'    => 'Bing_empty_menu_fallback'
-						) ); ?>
-					</nav>
-					<ul class="control">
+						) ); */?>
+					</nav>-->
+					<!--<ul class="control">
 						<li class="previous">
-							<a class="dashicons dashicons-arrow-left-alt" href="javascript:history.go( -1 );" title="<?php esc_attr_e( '后退', 'Bing' ); ?>"></a>
+							<a class="dashicons dashicons-arrow-left-alt" href="javascript:history.go( -1 );" title="<?php /*esc_attr_e( '后退', 'Bing' ); */?>"></a>
 						</li>
 						<li class="next">
-							<a class="dashicons dashicons-arrow-right-alt" href="javascript:history.go( 1 );" title="<?php esc_attr_e( '前进', 'Bing' ); ?>"></a>
+							<a class="dashicons dashicons-arrow-right-alt" href="javascript:history.go( 1 );" title="<?php /*esc_attr_e( '前进', 'Bing' ); */?>"></a>
 						</li>
 						<li class="refresh">
-							<a class="dashicons dashicons-image-rotate" href="javascript:location.reload();" title="<?php esc_attr_e( '刷新', 'Bing' ); ?>"></a>
+							<a class="dashicons dashicons-image-rotate" href="javascript:location.reload();" title="<?php /*esc_attr_e( '刷新', 'Bing' ); */?>"></a>
 						</li>
 					</ul>
-					<?php get_search_form(); ?>
+					--><?php /*get_search_form(); */?>
 				</div>
 				<?php /*Bing_mobile_header(); */?>
 			</header>
