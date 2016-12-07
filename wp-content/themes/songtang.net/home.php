241d8983fb1abc8be0aca13b87b4493322e9fc7b
doDomoGu <kaka_71936410@qq.com>
2016-12-08 02:14:32 +0800

    将首页 banner轮播 增加至5个

--- wp-content/themes/songtang.net/home.php
+++ wp-content/themes/songtang.net/home.php
@@ -6,22 +6,33 @@
     $pics_1 = of_get_option('index_banner_pics_1');
     $pics_2 = of_get_option('index_banner_pics_2');
     $pics_3 = of_get_option('index_banner_pics_3');
+    $pics_4 = of_get_option('index_banner_pics_4');
+    $pics_5 = of_get_option('index_banner_pics_5');
     if($pics_1)
         $banner_pics[1]['p'] = $pics_1;
     if($pics_2)
         $banner_pics[2]['p'] = $pics_2;
     if($pics_3)
         $banner_pics[3]['p'] = $pics_3;
-
+    if($pics_4)
+        $banner_pics[4]['p'] = $pics_4;
+    if($pics_5)
+        $banner_pics[5]['p'] = $pics_5;
     $link_1 = of_get_option('index_banner_link_1');
     $link_2 = of_get_option('index_banner_link_2');
     $link_3 = of_get_option('index_banner_link_3');
+    $link_4 = of_get_option('index_banner_link_4');
+    $link_5 = of_get_option('index_banner_link_5');
     if($pics_1)
         $banner_pics[1]['l'] = $link_1;
     if($pics_2)
         $banner_pics[2]['l'] = $link_2;
     if($pics_3)
         $banner_pics[3]['l'] = $link_3;
+    if($pics_4)
+        $banner_pics[4]['l'] = $link_4;
+    if($pics_5)
+        $banner_pics[5]['l'] = $link_5;
 
 ?>
 <div id="content">
@@ -65,43 +76,30 @@
 
     <?php
         $template_url = get_template_directory_uri();
-        $mag_pics = [];
-        /*$mag_pics = [
+        $mag_pics = [
             1=>['p'=>$template_url."/static/img/home/magizine_01.png"],
             2=>['p'=>$template_url."/static/img/home/magizine_02.png"],
             3=>['p'=>$template_url."/static/img/home/magizine_03.png"],
-        ];*/
+        ];
         $mag_pics_1 = of_get_option('index_mag_pics_1');
         $mag_pics_2 = of_get_option('index_mag_pics_2');
         $mag_pics_3 = of_get_option('index_mag_pics_3');
-        $mag_pics_4 = of_get_option('index_mag_pics_4');
-        $mag_pics_5 = of_get_option('index_mag_pics_5');
         if($mag_pics_1)
             $mag_pics[1]['p'] = $mag_pics_1;
         if($mag_pics_2)
             $mag_pics[2]['p'] = $mag_pics_2;
         if($mag_pics_3)
             $mag_pics[3]['p'] = $mag_pics_3;
-        if($mag_pics_4)
-            $mag_pics[4]['p'] = $mag_pics_4;
-        if($mag_pics_5)
-            $mag_pics[5]['p'] = $mag_pics_5;
 
         $mag_link_1 = of_get_option('index_mag_link_1');
         $mag_link_2 = of_get_option('index_mag_link_2');
         $mag_link_3 = of_get_option('index_mag_link_3');
-        $mag_link_4 = of_get_option('index_mag_link_4');
-        $mag_link_5 = of_get_option('index_mag_link_5');
         if($mag_pics_1)
             $mag_pics[1]['l'] = $mag_link_1;
         if($mag_pics_2)
             $mag_pics[2]['l'] = $mag_link_2;
         if($mag_pics_3)
             $mag_pics[3]['l'] = $mag_link_3;
-        if($mag_pics_4)
-            $mag_pics[4]['l'] = $mag_link_4;
-        if($mag_pics_5)
-            $mag_pics[5]['l'] = $mag_link_5;
     ?>
 
     <div id="pic_mag" >
@@ -136,28 +134,6 @@
                     </a>
                 <?php endif;?>
             </div>
-            <?php if(isset($mag_pics[4]['p']) && $mag_pics[4]['p']!=''):?>
-            <div class="mag_div mag_04">
-                <?php if(isset($mag_pics[4]['l']) && $mag_pics[4]['l']!=''):?>
-                    <a href="<?=$mag_pics[4]['l']?>" target="_blank" />
-                <?php endif;?>
-                <img src="<?=$mag_pics[4]['p']?>" />
-                <?php if(isset($mag_pics[4]['l']) && $mag_pics[4]['l']!=''):?>
-                    </a>
-                <?php endif;?>
-            </div>
-            <?php endif;?>
-            <?php if(isset($mag_pics[5]['p']) && $mag_pics[5]['p']!=''):?>
-                <div class="mag_div mag_05">
-                    <?php if(isset($mag_pics[5]['l']) && $mag_pics[5]['l']!=''):?>
-                        <a href="<?=$mag_pics[5]['l']?>" target="_blank" />
-                    <?php endif;?>
-                    <img src="<?=$mag_pics[5]['p']?>" />
-                    <?php if(isset($mag_pics[5]['l']) && $mag_pics[5]['l']!=''):?>
-                        </a>
-                    <?php endif;?>
-                </div>
-            <?php endif;?>
         </div>
     </div>
 </div>
