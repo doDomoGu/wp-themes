b55810d60410ea9c5d54e4106479645699177b7c
doDomoGu <kaka_71936410@qq.com>
2017-04-20 07:03:20 +0800

    根据 post_id_in_english 找到对应的英文文章

--- wp-content/themes/songtang.net/page_blank_20170319.php
+++ wp-content/themes/songtang.net/page_blank_20170319.php
@@ -13,7 +13,15 @@ Template Name:单页面_空白_20170319
     </div>
     <div id="main" class="container">
         <div id="post_content">
-        <?=$post->post_content?>
+            <?php
+            if(WP_THEME_LANG == 'en'){
+                $postInEnglish = get_post($post->post_id_in_english);
+                $post_content = $postInEnglish->post_content;
+            }else{
+                $post_content = $post->post_content;
+            }
+            ?>
+        <?=$post_content?>
         </div>
     </div>
 </div>
