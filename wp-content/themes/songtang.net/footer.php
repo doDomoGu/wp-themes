f33085d7aff00fc87402045b0fd7f635e3176f4d
doDomoGu <kaka_71936410@qq.com>
2019-04-22 13:57:25 +0800

    footer 备案用

--- wp-content/themes/songtang.net/footer.php
+++ wp-content/themes/songtang.net/footer.php
@@ -4,7 +4,7 @@
             <img src="<?php bloginfo('template_url');?>/static/img/footer.png" />
         </div>
         <div class="copyright">
-            Copyright &copy; 1993 - <?=date('Y')?> <strong><a href="<?php bloginfo('siteurl'); ?>/"><?php if(WP_THEME_LANG=='cn')bloginfo('name');else{echo 'China\'s Glory United';}?></a></strong> . All rights reserved
+            Copyright &copy; 1993 - <?=date('Y')?> <!--<strong><a href="<?php bloginfo('siteurl'); ?>/"><?php if(WP_THEME_LANG=='cn')bloginfo('name');else{echo 'China\'s Glory United';}?></a></strong>--> <b>苏州颂唐房地产咨询有限公司</b> 版权所有 <a href="http://www.beian.miit.gov.cn" target="_blank" >苏ICP备09006437号-3</a>
         </div>
     </div>
 </footer>
@@ -14,3 +14,8 @@
 ?>
 </body>
 </html>
+
+
+
+苏州颂唐房地产咨询有限公司
+
