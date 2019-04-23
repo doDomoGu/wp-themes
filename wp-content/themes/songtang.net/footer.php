628ce21a2aeb4ce041783808546b5cfd94b721e8
doDomoGu <kaka_71936410@qq.com>
2019-04-23 13:33:34 +0800

    更新备案网站地址

--- wp-content/themes/songtang.net/footer.php
+++ wp-content/themes/songtang.net/footer.php
@@ -4,7 +4,7 @@
             <img src="<?php bloginfo('template_url');?>/static/img/footer.png" />
         </div>
         <div class="copyright">
-            Copyright &copy; 1993 - <?=date('Y')?> <!--<strong><a href="<?php bloginfo('siteurl'); ?>/"><?php if(WP_THEME_LANG=='cn')bloginfo('name');else{echo 'China\'s Glory United';}?></a></strong>--> <b>苏州颂唐房地产咨询有限公司</b> 版权所有 <a href="http://www.beian.miit.gov.cn" target="_blank" >苏ICP备09006437号-3</a>
+            Copyright &copy; 1993 - <?=date('Y')?> <!--<strong><a href="<?php bloginfo('siteurl'); ?>/"><?php if(WP_THEME_LANG=='cn')bloginfo('name');else{echo 'China\'s Glory United';}?></a></strong>--> <b>苏州颂唐房地产咨询有限公司</b> 版权所有 <a href="http://js.beian.miit.gov.cn" target="_blank" >苏ICP备09006437号-3</a>
         </div>
     </div>
 </footer>
