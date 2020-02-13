18a5029bf7ac3878daf65a838146cdb71f323d1c
doDomoGu <kaka_71936410@qq.com>
2020-02-13 10:41:20 +0800

    foot logo

--- wp-content/themes/songtang.net/footer.php
+++ wp-content/themes/songtang.net/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer">
     <div class="container clearfix">
         <div class="footer-content">
-            <img src="<?php bloginfo('template_url');?>/static/img/footer.png" />
+            <img src="<?php bloginfo('template_url');?>/static/img/foot-logo.png" />
         </div>
         <div class="copyright">
             Copyright &copy; 1993 - <?=date('Y')?> <!--<strong><a href="<?php bloginfo('siteurl'); ?>/"><?php if(WP_THEME_LANG=='cn')bloginfo('name');else{echo 'China\'s Glory United';}?></a></strong>--> <b>苏州颂唐房地产咨询有限公司</b> 版权所有 <a href="http://www.beian.miit.gov.cn" target="_blank" >苏ICP备09006437号-3</a>
