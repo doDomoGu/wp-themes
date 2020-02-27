cad34b3288cddaca3c847e83904070bd2bf3800b
doDomoGu <kaka_71936410@qq.com>
2020-02-27 09:52:31 +0800

    底部 文字样式

--- wp-content/themes/songtang.net/footer.php
+++ wp-content/themes/songtang.net/footer.php
@@ -3,7 +3,7 @@
         <div class="footer-content">
             <img src="<?php bloginfo('template_url');?>/static/img/foot-logo.png" />
         </div>
-        <div class="copyright">
+        <div class="copyright" style="font-size:12px;color:#666;">
             Copyright &copy; 1993 - <?=date('Y')?> <!--<strong><a href="<?php bloginfo('siteurl'); ?>/"><?php if(WP_THEME_LANG=='cn')bloginfo('name');else{echo 'China\'s Glory United';}?></a></strong>--> <b>苏州颂唐房地产咨询有限公司</b> 版权所有 <a href="http://www.beian.miit.gov.cn" target="_blank" >苏ICP备09006437号-3</a>
         </div>
     </div>
